e826fec462f6040cec1d450cfa43f7252fcd5509
Omkar <omkar.mahajan872@gmail.com>
2021-12-04 12:13:53 +0530

    made clients model

--- clients/models.py
+++ clients/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class clients_model(models.Model):
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    globalScore = models.BigIntegerField()
+    hoursPracticed = models.IntegerField()
+    
